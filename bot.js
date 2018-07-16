f88d5f3ebc3fa1b9a23305f8ae6f4f111b0b2fa8
twaq <41275265+twaq@users.noreply.github.com>
2018-07-16 05:22:24 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,59 +1,35 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
+const Music = require('discord.js-musicbot-addon');
 client.on('ready', () => {
     console.log('I am ready!');
 });
 
-
-client.on("message", message => {  
-var prefix = ".";
-if(!message.channel.guild) return;
-if(message.content.startsWith(prefix + 'bc')) {
-if(!message.channel.guild) return message.channel.send('**هذا الأمر فقط للسيرفرات**').then(m => m.delete(5000));
-if(!message.member.hasPermission('ADMINISTRATOR')) return      message.channel.send('**للأسف لا تمتلك صلاحية** `ADMINISTRATOR`' );
-let args = message.content.split(" ").join(" ").slice(2 + prefix.length);
-let BcList = new Discord.RichEmbed()
-.setThumbnail(message.author.avatarURL)
-.setAuthor(`محتوى الرساله ${args}`)
-.setDescription(`برودكاست بـ امبد 📝\nبرودكاست بدون امبد✏ \nلديك دقيقه للأختيار قبل الغاء البرودكاست\nيمكنك اضافة اسم السيرفر في البرودكاست عن طريق كتابة <server>\nيمكنك اضافة اسم المرسل في البرودكاست عن طريق كتاية <by>\nيمكنك منشنة العضو في الرساله عن طريق كتابة <user>`)
-if (!args) return message.reply('**يجب عليك كتابة كلمة او جملة لإرسال البرودكاست**');message.channel.send(BcList).then(msg => {
-msg.react('📝')
-.then(() => msg.react('✏'))
-.then(() =>msg.react('📝'))
- 
-let EmbedBcFilter = (reaction, user) => reaction.emoji.name === '📝' && user.id === message.author.id;
-let NormalBcFilter = (reaction, user) => reaction.emoji.name === '✏' && user.id === message.author.id;
- 
-let EmbedBc = msg.createReactionCollector(EmbedBcFilter, { time: 60000 });
-let NormalBc = msg.createReactionCollector(NormalBcFilter, { time: 60000 });
- 
- 
-EmbedBc.on("collect", r => {
- 
-message.channel.send(`:ballot_box_with_check: تم ارسال الرساله بنجاح`).then(m => m.delete(5000));
-message.guild.members.forEach(m => {
-let EmbedRep = args.replace('<server>' ,message.guild.name).replace('<user>', m).replace('<by>', `${message.author.username}#${message.author.discriminator}`)
-var bc = new
-Discord.RichEmbed()
-.setColor('RANDOM')
-.setDescription(EmbedRep)
-.setThumbnail(message.author.avatarURL)
-m.send({ embed: bc })
-msg.delete();
+const client = new Discord.Client({disableEveryone: true})
+client.on('ready', ()=> {
+    console.log('Ready!')
 })
-})
-NormalBc.on("collect", r => {
-  message.channel.send(`:ballot_box_with_check: تم ارسال الرساله بنجاح`).then(m => m.delete(5000));
-message.guild.members.forEach(m => {
-let NormalRep = args.replace('<server>' ,message.guild.name).replace('<user>', m).replace('<by>', `${message.author.username}#${message.author.discriminator}`)
-m.send(NormalRep);
-msg.delete();
-})
-})
-})
-}
-});
-
+ Music.start(client, {
+    prefix: "!", // البرفكس هنا
+    youtubeKey: 'AIzaSyDIv8O9OwZdoTaghK3_SQmyGcQFzkXMW6E', //هنا ال yt v3 api key
+    global: false, // يكون للسيرفرات ؟ خليه false, يكون queue مشترك خليه true            
+    maxQueueSize: 50, // اقصى حجم لل queue        
+    playCmd: 'play', // امر التشغيل
+    volumeCmd: 'vol',     // امر الصوت
+    thumbnailType: 'high',
+    leaveCmd: 'stop',    // امر المغادرة
+    anyoneCanSkip: true, // تبي الكل يقدر يسوي skip? true > yep, false > nope
+    disableLoop: false, // تبي يكون فيه اعادة؟ , true > yep, false > nope
+    searchCmd: 'search', //امر البحث 
+    requesterName: true, 
+    inlineEmbeds: false,     
+    queueCmd: 'queue', // امر القائمة
+    pauseCmd: 'pause', // امر الايقاف المؤقت
+    resumeCmd: 'resume', // امر الاستمرار
+    skipCmd: 'skip', // امر السكب
+    loopCmd: 'loop', // امر الاعادة
+    enableQueueStat: true,
+  });
 
 
 client.login(process.env.BOT_TOKEN);
